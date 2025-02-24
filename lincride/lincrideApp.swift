8742781eb254e8c30e57506af3ae12f2cb604a93
Adeoluwa <a.adejobi@maontech.com>
2025-02-24 19:55:35 +0100

    Initial Commit

---
+++ lincride/lincrideApp.swift
@@ -0,0 +1,20 @@
+//
+//  lincrideApp.swift
+//  lincride
+//
+//  Created by Adeoluwa on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct lincrideApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ lincrideTests/lincrideTests.swift
@@ -0,0 +1,17 @@
+//
+//  lincrideTests.swift
+//  lincrideTests
+//
+//  Created by Adeoluwa on 24/02/2025.
+//
+
+import Testing
+@testable import lincride
+
+struct lincrideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
